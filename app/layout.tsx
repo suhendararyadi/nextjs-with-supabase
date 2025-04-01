88950329bc7d3c1b480041abe971e1d2901a9f01
suhendararyadi <suhendararyadi@gmail.com>
2025-04-01 07:44:39 +0700

    edit header

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,12 +41,12 @@ export default function RootLayout({
             <div className="flex-1 w-full flex flex-col gap-20">
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
+                  {/* <div className="flex gap-5 items-center font-semibold">
                     <Link href={"/"}>Next.js Supabase Starter</Link>
                     <div className="flex items-center gap-2">
                       <DeployButton />
                     </div>
-                  </div>
+                  </div> */}
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
               </nav>
@@ -54,7 +54,7 @@ export default function RootLayout({
                 {children}
               </div>
 
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+              {/* <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
                 <p>
                   Powered by{" "}
                   <a
@@ -67,7 +67,7 @@ export default function RootLayout({
                   </a>
                 </p>
                 <ThemeSwitcher />
-              </footer>
+              </footer> */}
             </div>
           </main>
         </ThemeProvider>
