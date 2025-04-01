73e3db7dd4f9ff80c24289f031766eb6bfa371f4
suhendararyadi <suhendararyadi@gmail.com>
2025-04-01 07:37:35 +0700

    edit layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,10 +37,10 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              {/* <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+          <main className="min-h-screen flex flex-col">
+            <div className="flex-1 w-full flex flex-col gap-20">
+              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+                <div className="w-full flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
                     <Link href={"/"}>Next.js Supabase Starter</Link>
                     <div className="flex items-center gap-2">
@@ -49,12 +49,12 @@ export default function RootLayout({
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
-              </nav> */}
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
+              </nav>
+              <div className="w-full p-5">
                 {children}
               </div>
 
-              {/* <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
                 <p>
                   Powered by{" "}
                   <a
@@ -67,7 +67,7 @@ export default function RootLayout({
                   </a>
                 </p>
                 <ThemeSwitcher />
-              </footer> */}
+              </footer>
             </div>
           </main>
         </ThemeProvider>
