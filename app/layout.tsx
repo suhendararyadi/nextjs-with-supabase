83eae11d81d526339970d2e85939855554981370
suhendararyadi <suhendararyadi@gmail.com>
2025-04-01 08:17:33 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import DeployButton from "@/components/deploy-button";
+// DeployButton removed
 import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
@@ -40,13 +40,7 @@ export default function RootLayout({
           <main className="min-h-screen flex flex-col">
             <div className="flex-1 w-full flex flex-col gap-20">
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
-                  </div>
+                <div className="w-full flex justify-end items-center p-3 px-5 text-sm">
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
               </nav>
@@ -54,7 +48,7 @@ export default function RootLayout({
                 {children}
               </div>
 
-              {/* <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
                 <p>
                   Powered by{" "}
                   <a
@@ -67,7 +61,7 @@ export default function RootLayout({
                   </a>
                 </p>
                 <ThemeSwitcher />
-              </footer> */}
+              </footer>
             </div>
           </main>
         </ThemeProvider>
