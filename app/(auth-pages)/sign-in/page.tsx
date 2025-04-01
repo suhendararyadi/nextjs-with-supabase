5a2a8f0a7c823754cc4661cfd53d23b4ec1f0a74
suhendararyadi <suhendararyadi@gmail.com>
2025-04-01 09:19:57 +0700

    memperbaiki posisi halaman login

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -2,8 +2,8 @@ import { LoginForm } from "@/components/login-form"
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-3xl">
+    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-muted">
+      <div className="w-full max-w-sm md:max-w-3xl px-4 py-8">
         <LoginForm />
       </div>
     </div>
