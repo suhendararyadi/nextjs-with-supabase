47970a331bbaf2adfb528620d0de6fe9a0d98ef7
suhendararyadi <suhendararyadi@gmail.com>
2025-04-01 09:23:48 +0700

    update login

---
+++ app/(auth-pages)/sign-in/layout.tsx
@@ -0,0 +1,28 @@
+import { Geist } from "next/font/google";
+import { ThemeProvider } from "next-themes";
+
+const geistSans = Geist({
+  display: "swap",
+  subsets: ["latin"],
+});
+
+export default function LoginLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={geistSans.className} suppressHydrationWarning>
+      <body className="bg-background text-foreground">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -3,7 +3,7 @@ import { LoginForm } from "@/components/login-form"
 export default function LoginPage() {
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center bg-muted">
-      <div className="w-full max-w-sm md:max-w-3xl px-4 py-8">
+      <div className="w-full max-w-sm md:max-w-3xl px-4">
         <LoginForm />
       </div>
     </div>
