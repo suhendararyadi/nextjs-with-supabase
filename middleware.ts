8adc9c73feee0df3ed67b0da8d40aeb6fe1d5729
suhendararyadi <suhendararyadi@gmail.com>
2025-04-01 07:13:51 +0700

    edit sidebar

--- middleware.ts
+++ middleware.ts
@@ -27,7 +27,7 @@ export async function middleware(request: NextRequest) {
     requestUrl.pathname.startsWith('/sign-up') ||
     requestUrl.pathname.startsWith('/forgot-password')
   )) {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
+    return NextResponse.redirect(new URL('/protected', request.url));
   }
 
   return response;
