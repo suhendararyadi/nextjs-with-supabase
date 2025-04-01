7a73175d471a754158dd920ddbca2d14522e7f35
suhendararyadi <suhendararyadi@gmail.com>
2025-04-01 09:02:49 +0700

    update

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -50,7 +50,7 @@ export function LoginForm({
               <SubmitButton pendingText="Signing In..." className="w-full">
                 Login
               </SubmitButton>
-              <FormMessage message={message} />
+              {message && <FormMessage message={message} />}
               <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                 <span className="relative z-10 bg-background px-2 text-muted-foreground">
                   Or continue with
