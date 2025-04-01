a01751f30a1b0c3ab92a04ed5156c2856ec4b5ce
suhendararyadi <suhendararyadi@gmail.com>
2025-04-01 08:04:52 +0700

    edit layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,12 +41,12 @@ export default function RootLayout({
             <div className="flex-1 w-full flex flex-col gap-20">
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full flex justify-between items-center p-3 px-5 text-sm">
-                  {/* <div className="flex gap-5 items-center font-semibold">
+                  <div className="flex gap-5 items-center font-semibold">
                     <Link href={"/"}>Next.js Supabase Starter</Link>
                     <div className="flex items-center gap-2">
                       <DeployButton />
                     </div>
-                  </div> */}
+                  </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
               </nav>
